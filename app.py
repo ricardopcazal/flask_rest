895774087406f5fc41d0a3065c495cee2d3638dd
ricardo <ricardopcazal@gmail.com>
2020-04-29 16:38:29 -0500

    add gitignore

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
-from db import db
 from resources.item import Item, ItemList
 from resources.user import UserRegister
 from resources.store import Store, StoreList
@@ -11,14 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
-db.init_app(app)
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)
 
@@ -29,4 +21,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
